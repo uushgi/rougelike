e39b1f7a87415b86343b17e9f12b1da0e7a940d5
uushgi <majjiima@gmail.com>
2025-04-12 21:42:08 +1000

    Update main.py
    basic game logic and some inventory tweaks (mostly for damage calculation via active slot)

--- src/main.py
+++ src/main.py
@@ -10,25 +10,15 @@ class Character:
         self.position = position        
         self.inventory = Inventory(5)
 
-    def attack_character(self, character):
-        total_damage = 0
-        for i in self.inventory.items:
-            if isinstance(i, Sword):
-                total_damage += i.damage
-        
-        character.health -= total_damage
-
-    def get_state(self):
-        return f"Здоровье: {self.health}/{self.max_health}"
-
 class Hero(Character):
     def __init__(self, health, position, symbol):
         super().__init__(health, position)
         self.symbol = symbol
 
 class Enemy(Character):
-    def __init__(self, health, position, symbol):
+    def __init__(self, health, damage, position, symbol):
         super().__init__(health, position)
+        self.damage = damage
         self.symbol = symbol
 
 class Item:
@@ -45,7 +35,94 @@ class Inventory:
     def __init__(self, size, items=[]):
         self.items = items
         self.size = size
+        self.active_slot = None
     
     def add_item(self, item):
         if len(self.items) < self.size:
             self.items.append(item)
+
+    def equip_sword(self, item):
+        if isinstance(item, Sword):
+            self.active_slot = item
+            print(f"Экипирован {item.title}")
+        else:
+            print(f"Не удалось экипировать предмет")
+
+class Logic:
+    def __init__(self, player, enemies):
+        self.player = player
+        self.enemies = enemies
+        self.actions = Actions()
+        self.turns = Turns(player, enemies)
+        self.state = State()
+    
+    def game_loop(self):
+        while True:
+            current = self.turns.current_turn()
+            
+            if current == self.player:
+                self.player_turn_manager()
+            else:
+                for enemy in self.enemies:
+                    if enemy.health > 0:
+                        self.enemy_turn_manager(enemy)
+
+            self.turns.next_turn()
+
+            if self.state.is_player_dead(self.player):
+                print(f"смэрть")
+                break
+
+            if self.state.all_enemies_are_dead(self.enemies):
+                print(f"победа")
+                break
+    
+    def player_turn_manager(self):
+        print(f"\n[Ход игрока {self.player.symbol}]")
+        print(f"Здоровье: {self.player.health}/{self.player.max_health}")
+        for enemy in self.enemies:
+            if enemy.health > 0:
+             self.actions.attack_character(self.player, enemy)
+             break
+
+    def enemy_turn_manager(self, enemy):
+        print(f"\n[Ход врага {enemy.symbol}]")
+        print(f"Здоровье врага: {enemy.health}")
+        self.actions.attack_character(enemy, self.player)
+
+class Turns:
+    def __init__(self, player, enemies):
+        self.player = player
+        self.enemies = enemies
+        self.is_player_turn = True
+
+    def next_turn(self):
+        self.is_player_turn = not self.is_player_turn
+
+    def current_turn(self):
+        if self.is_player_turn:
+            return self.player
+        return self.enemies
+    
+class Actions:
+    def movement(self, character, direction):
+        print(f"{character} двигается на {direction}")  # заглушка. игрок/моб двигается на север/юг/запад/восток
+
+    def attack_character(self, attacker, target):
+        damage = self.total_damage(attacker)
+        target.health -= damage
+        print(f"{attacker} нанес {target} {damage} урона")
+
+    def total_damage(self, character):
+        if isinstance(character, Hero):
+            if character.inventory.active_slot:
+                return character.inventory.active_slot.damage
+            return 2 # стандартный урон игрового персонажа, условно кулаки
+        return character.damage # урон врагов
+    
+class State:
+    def is_player_dead(self, hero):
+        return hero.health <= 0
+    
+    def all_enemies_are_dead(self, enemies):
+        return all(enemy.health <= 0 for enemy in enemies)
